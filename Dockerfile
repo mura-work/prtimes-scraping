4e291cc6eb43aed863a6d61c0594f9ef443f259a
mura-work <murata.naoya.work@gmail.com>
2023-06-25 12:48:54 +0900

    [fix] schedule.rbを本番環境用に修正

--- Dockerfile
+++ Dockerfile
@@ -7,13 +7,16 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.lis
 RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
 RUN echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google.list
 RUN apt-get update -qq
-RUN apt-get install -y nodejs build-essential libpq-dev yarn google-chrome-stable libnss3 libgconf-2-4 vim
+RUN apt-get install -y nodejs build-essential libpq-dev yarn google-chrome-stable libnss3 libgconf-2-4 vim cron
 
 # ChromeDriver のインストール
 # 現在の最新のバージョンを取得し、それをインストールする。
 RUN wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
 RUN unzip chromedriver_linux64.zip && rm chromedriver_linux64.zip
 RUN mv chromedriver /usr/local/bin/
+
+# cronの起動
+RUN service cron start
 
 RUN mkdir /prtimes-scraping
 WORKDIR /prtimes-scraping
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,33 +1,21 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# require File.expand_path(File.dirname(__FILE__) + "/environment")
+# set :output, 'log/cron.log' # ログの出力先ファイルを設定
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+rails_env = ENV['RAILS_ENV'] ||= 'production'
+set :output, 'log/cron.log'
+set :environment, rails_env
+ENV.each { |k, v| env(k, v) }
 
-# Learn more: http://github.com/javan/whenever
-
-require File.expand_path(File.dirname(__FILE__) + "/environment")
-set :output, 'log/cron.log' # ログの出力先ファイルを設定
-
-rails_env = Rails.env.to_sym
-set :environment, rails_env # 環境を設定
+# rails_env = Rails.env.to_sym
+# set :environment, rails_env # 環境を設定
 
 job_type :rake, "export PATH=\"$HOME/.rbenv/bin:$PATH\"; eval \"$(rbenv init -)\"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output"
 
+# バッチ処理の実行テスト
+# every 2.minute do
+#   rake "test_connect_spreadsheet:test"
+# end
+
 every 1.days, at: '1:00 am' do # 1日1回実行
-  ## rake "test_connect_spreadsheet:test"
   rake "scraping_latest_data:sheet"
 end
